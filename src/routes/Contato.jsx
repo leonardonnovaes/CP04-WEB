11f0e06e920c8de3d08576b846e3581a21b67f91
Miguel Carmo <migielcarmo21@gmail.com>
2024-09-07 16:30:58 -0300

    Criando contatos

--- src/routes/Contato.jsx
+++ src/routes/Contato.jsx
@@ -1,10 +1,28 @@
 function Contato() {
-    return (
-      <div className="contato">
-        <h2>Contato</h2>
-        <p>Entre em contato conosco pelo telefone (11) 1234-5678 ou pelo email contato@lojadesportes.com.</p>
+  return (
+    <div className="contato">
+      <h2>Contato</h2>
+      <div className="contato-form">
+        <form action="">
+          <div className="formControl1">
+            <label htmlFor="">Usuario</label>
+            <input type="text" placeholder="Digite seu usuário" className="Caixa1"/>
+          </div>
+          <div className="formControl1">
+            E-mail:
+            <input type="email" placeholder="Digite seu e-mail" className="Caixa1"/>
+          </div>
+          <div className="formControl1">
+            Mensagem:
+            <textarea placeholder="Digite sua mensagem" className="Caixa2" />
+          </div>
+          <p>
+            <button type="submit">Enviar</button>
+          </p>
+        </form>
       </div>
-    );
-  }
-  
-  export default Contato;
+    </div>
+  );
+}
+
+export default Contato;
